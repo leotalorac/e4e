dc3fba93bf871b0981254b34f4ebe9060d0991dc
Camilo Villalobos <camilovillalobos@la-MacBook-de-Camilo.local>
2019-07-07 01:20:56 -0500

    bluetoohd proves

--- e4e/components/Alert.js
+++ e4e/components/Alert.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View,TextInput,TouchableOpacity,ImageBackground,Image} from 'react-native';
-
+import BluetoothSerial from "react-native-bluetooth-serial";
 export default class Alert extends React.Component{
     static navigationOptions = {
         title: 'Estado de emergencia',
     };
+    componentWillMount() {
+        Promise.all([BluetoothSerial.isEnabled(), BluetoothSerial.list()]).then(
+          values => {
+            const [isEnabled, devices] = values;
+            this.setState({ isEnabled, devices, devicesFormatted });
+          }
+        );
+    
+        BluetoothSerial.on("bluetoothEnabled", () =>
+          console.log("Bluetooth enabled")
+        );
+    
+        BluetoothSerial.on("bluetoothDisabled", () =>
+          console.log("Bluetooth disabled")
+        );
+    
+        BluetoothSerial.on("error", err => {
+          console.log("error", err);
+        });
+    
+        BluetoothSerial.on("connectionLost", () => {
+          if (this.state.device) {
+            this.connect(this.state.device)
+              .then(res => {})
+              .catch(err => {
+                console.log("error", err);
+              });
+          }
+        });
+      }
     render(){
         const {navigate} = this.props.navigation;
         return(
